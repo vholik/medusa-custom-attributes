44e947dec7ce8a12151b7dbe79e6aea82dfd1654
Viktor Holik <1viktorholik@gmail.com>
2023-11-06 16:00:20 +0100

    Remove unneccessary console.log

--- src/services/product.ts
+++ src/services/product.ts
@@ -84,8 +84,6 @@ class ProductService extends MedusaProductService {
 
     const { q, query, relations } = this.prepareListQuery_(selector, config);
 
-    console.log(selector);
-
     // @ts-ignore
     query.where.attributes_id = selector.attributes_id;
     // @ts-ignore
